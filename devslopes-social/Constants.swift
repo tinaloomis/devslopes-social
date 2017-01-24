00f4b1388dd3691ee0454aab06be484764bbd125
tinaloomis <tina.loomis@gmx.com>
2017-01-24 12:10:51 -0800

    Added Constants and Styling

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Tina Dimler on 1/24/17.
+//  Copyright © 2017 Tina Loomis. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0 / 255.0
